d834cc5f913402f9dc7e78b902ca42bd2bdc255b
nivbn <nivbnr@gmail.com>
2026-01-07 19:59:40 +0200

    delete demos

--- app/demos/00_blank/page.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./page.module.css";
-
-export default function Demos() {
-  return (
-    <div className={styles.pageContainer}>
-      <h1 className={styles.hey}>Hey</h1>
-    </div>
-  );
-}
